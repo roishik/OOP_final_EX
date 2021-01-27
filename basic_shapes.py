f43f6ebcecb99e347c037b97aa58056e236961c7
Gil Ben David <you@example.com>
2021-01-27 17:09:45 +0200

    finish wo Wendsday. all the draw functions done

--- basic_shapes.py
+++ basic_shapes.py
@@ -2,10 +2,11 @@ from shape import *
 import numpy as np
 import cv2 as cv
 
+DEFAULT_LINE_WIDTH = 5
+
 
 class Point(Shape):
     DEFAULT_POINT_SIZE = 5
-    DEFAULT_LINE_WIDTH = 5
 
     def __init__(self, x, y):
         self.x = x
@@ -13,13 +14,15 @@ class Point(Shape):
         self.radius = self.DEFAULT_POINT_SIZE
 
     def draw(self, img):
-        drawing_params = {'canvas': img, 'center': self.as_tuple, 'radius': self.DEFAULT_POINT_SIZE}
+        drawing_params = {'canvas': img,
+                          'center': self.as_tuple,
+                          'radius': self.DEFAULT_POINT_SIZE}
         cv.circle(*drawing_params.values(), self.fill_color.cv_color, -1)  # plot shape
-        cv.circle(*drawing_params.values(), self.line_color.cv_color, self.DEFAULT_LINE_WIDTH)  # plot line
+        cv.circle(*drawing_params.values(), self.line_color.cv_color, DEFAULT_LINE_WIDTH)  # plot line
 
     @property
     def as_tuple(self):
-        return self.x, self.y
+        return tuple([self.x, self.y])
 
 
 class Line(Shape):
@@ -27,12 +30,26 @@ class Line(Shape):
         self.point_1 = point_1
         self.point_2 = point_2
 
+    def draw(self, img):
+        drawing_params = {'canvas': img,
+                          'point1': self.point_1.as_tuple,
+                          'point2': self.point_2.as_tuple}
+        cv.line(*drawing_params.values(), self.line_color.cv_color, thickness=DEFAULT_LINE_WIDTH)
+
 
 class Triangle(Shape):
     def __init__(self, point_1, point_2, point_3):
         self.point_1 = point_1
         self.point_2 = point_2
         self.point_3 = point_3
+
+    def draw(self, img):
+        Line(self.point_1, self.point_2).draw(img)
+        Line(self.point_1, self.point_3).draw(img)
+        Line(self.point_2, self.point_3).draw(img)
+        drawing_params = {'canvas': img,
+                          'points': [self.point_1.as_tuple, self.point_2.as_tuple, self.point_3.as_tuple]}
+        cv.drawContours(*drawing_params.values(), 0, self.fill_color.cv_color, -1)
 
 
 class Rectangle(Shape):
@@ -42,8 +59,22 @@ class Rectangle(Shape):
         self.bottom_left_point = Point(top_left_point.x, bottom_right_point.y)
         self.top_right_point = Point(bottom_right_point.x, top_left_point.y)
 
+    def draw(self, img):
+        drawing_params = {'canvas': img,
+                          'point1': self.top_left_point.as_tuple,
+                          'point2': self.bottom_right_point.as_tuple}
+        cv.rectangle(*drawing_params.values(), self.fill_color.cv_color, -1)  # plot shape
+        cv.rectangle(*drawing_params.values(), self.line_color.cv_color, DEFAULT_LINE_WIDTH)  # plot line
+
 
 class Circle(Shape):
     def __init__(self, center, radius):
         self.center = Point(center[0], center[1])
         self.radius = radius
+
+    def draw(self, img):
+        drawing_params = {'canvas': img,
+                          'center': self.center.as_tuple,
+                          'radius': self.radius}
+        cv.circle(*drawing_params.values(), self.fill_color.cv_color, -1)  # plot shape
+        cv.circle(*drawing_params.values(), self.line_color.cv_color, DEFAULT_LINE_WIDTH)  # plot line
--- main.py
+++ main.py
@@ -4,10 +4,9 @@ import cv2 as cv
 
 def main():
     img = np.random.rand(600, 900, 3)
-    params = [img, (200, 200), 63, (0, 255, 0)]
+    params = [img, tuple([200, 200]), 63, (0, 255, 0)]
     cv.circle(*params, 10)
-    cv.circle(img, (200, 200), 63, (255, 0, 0), -1)
-
+    cv.circle(img, tuple([200, 200]), 63, (255, 0, 0), -1)
 
     cv.imshow('RGB', img)
     cv.waitKey(0)
